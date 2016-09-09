067e91e35d6457e962a792f3f7461c27f59ffa4a
Phedora Johnson <phedoraj@gmail.com>
2016-09-09 19:50:41 -0400

    Completed

--- fundamentals-assessment-2.js
+++ fundamentals-assessment-2.js
@@ -2,3 +2,30 @@ var two = 2;
 var myArray = [1, two, "three", "4", 5, "six", "7", 8];
 
 //YOUR CODE BELOW
+var printAll = function (){
+	for (var i = 0; i < myArray.length; i++){
+
+	}
+	console.log(printAll[myArray[i]);
+
+};
+
+}
+
+var longArray = function () {
+	if (myArray.length >= 7);
+}
+
+} else {
+	return false;
+};
+
+var countNums = function (){
+	var count = 0
+	for(var i = 0; i < myArray.length; i++){
+		if(typeOf[myArray] === i)
+		return count++
+	}
+	
+	countNums(myArray); 
+};
